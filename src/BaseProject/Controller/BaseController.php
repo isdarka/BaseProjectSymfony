0988f44804a01d62431215000a524dbaff970bc4
isdarka <musicogui@gmail.com>
2014-01-03 13:18:23 -0600

    BaseController

---
+++ src/BaseProject/Controller/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @author isdarka
+ * @created Jan 3, 2014 1:15:32 PM
+ */
+
+namespace BaseProject\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BaseController extends Controller
+{
+	
+}
